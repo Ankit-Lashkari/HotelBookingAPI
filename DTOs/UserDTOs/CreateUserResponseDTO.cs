b188e8a6ec7143acd72f19b9a555ec1abc196ab9
ankit.lashkari <ankit.lashkari@YI1016156LT>
2025-07-10 18:28:58 +0530

    Add project files.

---
+++ DTOs/UserDTOs/CreateUserResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBookingAPI.DTOs.UserDTOs
+{
+    public class CreateUserResponseDTO
+    {
+        public int UserId { get; set; } // it was string i marked it to string
+        public string Message { get; set; }
+        public bool IsCreated { get; set; }
+
+    }
+}
